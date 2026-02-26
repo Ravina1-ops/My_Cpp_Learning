8f27c63e96213e98252aa85f70b780a708c3f493
Ravina <hema130384@gmail.com>
2026-02-26 21:37:46 +0530

    added some other programs

---
+++ Functions/Recursion/test.cpp
@@ -0,0 +1,5 @@
+#include<iostream>
+int main(){
+    std::cout<<"Button is working!";
+    return 0;
+}
---
+++ OOPS/DigitalClock.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class DigitalClock{
+    private:
+    int h,m,s;
+    public:
+    void display(int hour,int minutes,int seconds){
+        hour = h;
+        minutes = m;
+        seconds = s;
+    }
+    void time(int hour){
+    if(hour>10){
+    
+    }}
+};
+int main(){
+    int hour,minutes,seconds;
+    cout<<"Hour: ";
+   cin>>hour;
+    cout<<"Minutes: ";
+    cin>>minutes;
+    cout<<"Seconds: ";
+    cin>>seconds;
+    DigitalClock time(int hour,int minutes,int seconds);
+}
